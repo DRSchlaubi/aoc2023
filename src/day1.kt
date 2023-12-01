ffa375f7778bafa23742b08f5ad6d0f0429bc5ad
Michael Rittmeister <michael@rittmeister.in>
2023-12-01 09:26:37 +0100

    Day 1

---
+++ src/day1.kt
@@ -0,0 +1,59 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+fun main() {
+    val input = Path("inputs/input1.txt").readLines()
+
+    val result1 = input.processLines()
+    val result2 = input.processLines(String::transformWordNumbersToNumeric)
+
+
+    println("Result 1: $result1")
+    println("Result 2: $result2")
+}
+
+
+fun Iterable<String>.processLines(preProcess: String.() -> String = { this }) = sumOf {
+    val chars = preProcess(it).toCharArray()
+        .filter(Char::isDigit)
+
+    (chars.first().toString() + chars.last()).toInt()
+}
+
+fun String.transformWordNumbersToNumeric(): String {
+    val wordToNumberMapping = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9",
+        "zero" to "0"
+    )
+
+    return buildString {
+        val iterator = this@transformWordNumbersToNumeric.iterator()
+        var currentWord: String? = iterator.next().toString()
+        while (currentWord != null) {
+            if (currentWord.lastOrNull()?.isDigit() == true) {
+                append(currentWord.last())
+                currentWord = iterator.nextOrNull()?.toString()
+            } else {
+                if (wordToNumberMapping.none { (word) -> word.startsWith(currentWord!!) }) {
+                    currentWord = currentWord.drop(1)
+                } else {
+                    currentWord += iterator.nextOrNull() ?: break
+                }
+                val number = wordToNumberMapping[currentWord]
+                if (number != null) {
+                    append(number)
+                }
+            }
+        }
+    }
+}
+
+private fun <T> Iterator<T>.nextOrNull() = if (hasNext()) next() else null
