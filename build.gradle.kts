4a5c80117a21d4516f7c47576bc7621abdae6b0e
Michael Rittmeister <michael@rittmeister.in>
2023-12-02 16:17:29 +0100

    Add results to CI output

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,18 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
 
 tasks {
     withType<KotlinJvmTest> {
         useJUnitPlatform()
+
+        testLogging {
+            events(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.STANDARD_OUT)
+
+            exceptionFormat = TestExceptionFormat.FULL
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+        }
     }
 }
